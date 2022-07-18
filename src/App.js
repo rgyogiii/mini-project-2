398c48fdb7a0458f1a000f6b741d6f45d4219428
georgeromarrr <romar.doinog@gmail.com>
2022-07-18 21:09:40 +0800

    app template, package installation

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.min.js';
+import 'bootstrap-icons/font/bootstrap-icons.css';
+
+const App = () => {
+  return (
+    <div>App</div>
+  )
+}
+
+export default App
