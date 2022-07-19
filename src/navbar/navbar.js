c09110235145503937434589b3d1ff161bd3b1b6
georgeromarrr <romar.doinog@gmail.com>
2022-07-20 03:58:38 +0800

    navbar update

--- src/navbar/navbar.js
+++ src/navbar/navbar.js
@@ -1,48 +1,46 @@
 import React from 'react';
-import logo from '';
-
+import './navbar.css';
 
 const navbar = () => {
   return (
-    <div className="navbar navbar-expands-lg navbar-light">
+    <div className="navbar navbar-expands-lg navbar-light d-flex flex-row">
       <div className="container">
         
         {/* searchbox */}
-        <form className="d-flex m-0" role="search">
-          <input
-            className="form-control border border-0"
-            type="search"
-            placeholder="Search here..."
-            />
-
+        <form className="d-flex m-0 d-flex align-items-center px-1 me-auto searchContainer" role="search">
           <a href="###">
-            <button className="btn px-2">
+            <button className="btn pe-2 search-btn">
               <i className="bi bi-search"></i>
             </button>
           </a>
+          <input
+            className="form-control border border-0 searchbox px-1"
+            type="search"
+            placeholder="Search here..."
+            />
         </form>
 
         {/* logo */}
         <a className="navbar-brand" href="###">
-          <img src={logo} height="50" alt="Araktek Logo"/>
+          <img src={process.env.PUBLIC_URL+"logo.svg"} height="60" alt="Araktek Logo"/>
         </a>
 
         {/* navlist */}
-        <ul className="navbar-nav d-flex flex-row">
-          <li className="nav-item me-2">
+        <ul className="navbar-nav d-flex flex-row d-flex align-items-end ms-auto">
+          <li className="nav-item me-4">
             <a className="nav-link" href="###">
               <i className="bi bi-suit-heart"></i>
             </a>
           </li>
-          <li className="nav-item me-2">
+          <li className="nav-item me-4">
             <a className="nav-link" href="###">
               <i className="bi bi-cart3"></i>
             </a>
           </li>
-          <li className="nav-item me-2">
-            <a className="nav-link" href="###">
-              <i className="bi bi-person-circle"></i>
-              <i class="bi bi-chevron-down"></i>
+          <li className="nav-item">
+            <a className="nav-link d-flex align-items-center" href="###">
+              <i className="bi bi-person-circle me-0"></i>
+              <i className="bi bi-caret-down-fill ms-1"></i>
             </a>
           </li>
         </ul>
