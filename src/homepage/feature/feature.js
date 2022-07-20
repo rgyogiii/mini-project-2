869ce84543b7294f4e3c4c7c2335d3efb84060ba
jryambao <johnralph@gmail.com>
2022-07-20 21:57:37 +0800

    fixed

--- src/homepage/feature/feature.js
+++ src/homepage/feature/feature.js
@@ -69,7 +69,7 @@ const Final = () => {
               <div className='card-list mb-5 pb-3 '>
               <div className="image-hover">
         <div onMouseEnter={()=> showButton(i)} onMouseLeave={hideButton}>
-        <img src={item.imageName} width='280px' height='240px'/> <br/>
+        <div className='d-flex justify-content-center'><img src={item.imageName} width='250px' height='240px'/></div> <br/>
           {isHovered === i && (
             <div className='d-flex justify-content-start'><button className="mb-5 mx-2 btn btn-secondary btn-xs fw-bold" type="button" >Add to cart</button></div>
               )}<br />
@@ -78,7 +78,7 @@ const Final = () => {
         <div className='d-flex justify-content-end mx-3'>
           <i className="bi bi-heart fav-icon"></i>
         </div>
-        <span className='title fs-6 fw-bold px-1'>{item.title} </span>
+        <span className='title fw-bold px-1'>{item.title} </span>
         <div className="price">
           <div className='d-flex justify-content-between'>
           <span className='fs-6 px-1 fw-bold'>{item.price}</span>
