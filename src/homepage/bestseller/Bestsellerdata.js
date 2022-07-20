3355826900fb12c52fe375244662b7232d2f0697
Louie John Manuel Lauro <burshiki@gmail.com>
2022-07-20 19:09:05 +0800

    see more move to center and reshuffle data

--- src/homepage/bestseller/Bestsellerdata.js
+++ src/homepage/bestseller/Bestsellerdata.js
@@ -1,94 +1,6 @@
-const Bestsellerdata = [{
+const Bestsellerdata = [
+{
     id: 1,
-    title: 'PlayStation 5 HFW Bundle...',
-    price: "₱41,860.00",
-    imageName: "/img/consoles/console1.jpg",
-    
-    description: "to follow na lang since single page pa lang naman",
-    reviewNum: '(15)'
-},
-{
-    id: 2,
-    title: 'Monster Hunter Lioleia...',
-    price: "₱4,495.00",
-    imageName: "/img/collectibles/collectible1.jpg",
-    description: "to follow na lang since single page pa lang naman",
-    reviewNum: '(23)'
-},
-{
-    id: 3,
-    title: 'Lenovo Legion T5 ...',
-    price: "₱109,995.00",
-    imageName: "/img/prebuilt/prebuilt1.jpg",
-    description: "to follow na lang since single page pa lang naman",
-    reviewNum: '(64)'
-},
-{
-    id: 4,
-    title: 'Asus ROG Zephyrus G15 ...',
-    price: "₱139,995.00",
-    imageName: "/img/laptop/laptop4.jpg",
-    description: "to follow na lang since single page pa lang naman",
-    reviewNum: '(22)'
-},
-{
-    id: 5,
-    title: 'Nintendo Switch ...',
-    price: "₱17,895.00",
-    imageName: "/img/consoles/console6.jpg",
-    description: "to follow na lang since single page pa lang naman",
-    reviewNum: '(19)'
-},
-{
-    id: 6,
-    title: 'Xbox Series S ...',
-    price: "₱18,495.00",
-    imageName: "/img/consoles/console4.jpg",
-    description: "to follow na lang since single page pa lang naman",
-    reviewNum: '(87)'
-},
-{
-    id: 7,
-    title: 'Capcom Figure Builder...',
-    price: "₱2,950.00",
-    imageName: "/img/collectibles/collectible2.jpg",
-    description: "to follow na lang since single page pa lang naman",
-    reviewNum: '(96)'
-},
-{
-    id: 8,
-    title: 'Asus ROG Strix G15DH ...',
-    price: "₱54,995.00",
-    imageName: "/img/prebuilt/prebuilt5.jpg",
-    description: "to follow na lang since single page pa lang naman",
-    reviewNum: '(32)'
-},
-{
-    id: 9,
-    title: 'Ayaneo Next AMD Ryzen 7...',
-    price: "₱65,995.00",
-    imageName: "/img/consoles/console9.jpg",
-    description: "to follow na lang since single page pa lang naman",
-    reviewNum: '(62)'
-},
-{
-    id: 10,
-    title: 'Nintendo Amiibo ...',
-    price: "₱595.00",
-    imageName: "/img/collectibles/collectible8.jpg",
-    description: "to follow na lang since single page pa lang naman",
-    reviewNum: '(76)'
-},
-{
-    id: 11,
-    title: 'Lenovo Legion 5 ...',
-    price: "₱104,995.00",
-    imageName: "/img/laptop/laptop1.jpg",
-    description: "to follow na lang since single page pa lang naman",
-    reviewNum: '(21)'
-},
-{
-    id: 12,
     title: 'GDP Pocket 3 i7...',
     price: "₱54,995.00",
     imageName: "/img/consoles/console10.jpg",
@@ -96,39 +8,25 @@ const Bestsellerdata = [{
     reviewNum: '(10)'
 },
 {
-    id: 13,
-    title: 'Lenovo Legion Tower ...',
-    price: "₱79,995.00",
-    imageName: "/img/prebuilt/prebuilt3.jpg",
-    description: "to follow na lang since single page pa lang naman",
-    reviewNum: '(52)'
-},
-{
-    id: 14,
+    id: 2,
     title: 'NIER REPLICANT Action ...',
     price: "₱3,295.00",
     imageName: "/img/collectibles/collectible6.jpg",
     description: "to follow na lang since single page pa lang naman",
     reviewNum: '(62)'
 },
+
 {
-    id: 15,
-    title: 'Gigabyte A7 ...',
-    price: "₱99,995.00",
-    imageName: "/img/laptop/laptop9.jpg",
+    id: 3,
+    title: 'Lenovo Legion Tower ...',
+    price: "₱79,995.00",
+    imageName: "/img/prebuilt/prebuilt3.jpg",
     description: "to follow na lang since single page pa lang naman",
-    reviewNum: '(13)'
+    reviewNum: '(52)'
 },
+
 {
-    id: 16,
-    title: 'HP Victus 16 ...',
-    price: "₱59,995.00",
-    imageName: "/img/laptop/laptop6.jpg",
-    description: "to follow na lang since single page pa lang naman",
-    reviewNum: '(69)'
-},
-{
-    id: 17,
+    id: 4,
     title: 'Acer Predator Helios 300 ...',
     price: "₱74,999.00",
     imageName: "/img/laptop/laptop7.jpg",
@@ -136,7 +34,7 @@ const Bestsellerdata = [{
     reviewNum: '(16)'
 },
 {
-    id: 18,
+    id: 5,
     title: 'Acer Nitro 5 ...',
     price: "₱53,899.00",
     imageName: "/img/laptop/laptop8.jpg",
@@ -144,7 +42,7 @@ const Bestsellerdata = [{
     reviewNum: '(12)'
 },
 {
-    id: 19,
+    id: 6,
     title: 'Lenovo IdeaCentre 3...',
     price: "₱46,995.00",
     imageName: "/img/prebuilt/prebuilt6.jpg",
@@ -152,13 +50,21 @@ const Bestsellerdata = [{
     reviewNum: '(15)'
 },
 {
-    id: 20,
+    id: 7,
+    title: 'Gigabyte A7 ...',
+    price: "₱99,995.00",
+    imageName: "/img/laptop/laptop9.jpg",
+    description: "to follow na lang since single page pa lang naman",
+    reviewNum: '(13)'
+},
+{
+    id: 8,
     title: 'Trials of Mana...',
     price: "₱9,495.00",
     imageName: "/img/collectibles/collectible4.jpg",
     description: "to follow na lang since single page pa lang naman",
     reviewNum: '(13)'
-},
+}
 ]
 
 export default Bestsellerdata;
