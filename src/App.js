aae472ac56d474c526dc4cf1db2dd7073e0e76ef
georgeromarrr <romar.doinog@gmail.com>
2022-07-22 00:15:34 +0800

    minor revisions

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React from 'react'
 import Navbar from './navbar/navbar';
 import Homepage from './homepage/homepage';
-import Footer from './footer/Footer';
-import Productlist from './product/productlist/Productlist';
+import Footer from './footer/footer';
+import Productlist from './product/productlist/productlist';
 import  { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 
 function App() {
@@ -11,7 +11,7 @@ function App() {
     <Router>
       <Navbar />
       <div>
-        <img src={process.env.PUBLIC_URL+"/img/creatives/bg-podium.svg"} alt="header img"/>
+        <img src={process.env.PUBLIC_URL+"/img/creatives/bg-podium.svg"} width="100%" alt="header img"/>
       </div>
         <Routes>
           <Route path='/' element={<Homepage />} />
