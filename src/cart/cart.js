7821afdb47cb4c58e0b6618e81792c9f0ac985f8
jryambao <johnralph@gmail.com>
2022-07-22 17:10:12 +0800

    fixed cart buttons

--- src/cart/cart.js
+++ src/cart/cart.js
@@ -44,10 +44,10 @@ const Cart = () => {
                 <td>QTY:{item.quantity}</td>
 
                 <button className='btn btn-secondary ms-2' onClick={()=> updateItemQuantity(item.id, item.quantity -1)}>
-                -
+                <i class="bi bi-dash"></i>
                 </button>
                 <button className='btn btn-secondary ms-2' onClick={()=> updateItemQuantity(item.id, item.quantity +1)}>
-                +
+                <i class="bi bi-plus"></i>
                 </button>
                 <button className='btn btn-secondary ms-2' onClick={()=> removeItem(item.id)}>
                 <i class="bi bi-trash3-fill fs-6 trash"></i>
