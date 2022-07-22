c117f39e8df1c6ba6b4d6358f92ce56f72815105
jryambao <johnralph@gmail.com>
2022-07-22 14:02:32 +0800

    added promo page

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import Cart from './cart/cart'
 import { CartProvider } from 'react-use-cart'
 import Productlist from './product/productlist/productlist';
 import  { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
-import Cartpage from './cart/cartpage';
+import Promos from './promos/promos'
 
 const App = () => {
   return (
@@ -14,11 +14,12 @@ const App = () => {
     <Router>
       <CartProvider>
       <Navbar/>
-      <img src={process.env.PUBLIC_URL+"/img/creatives/bg-podium.svg"} width="100%" alt="headerimg"/>
+      
       <Routes>
         <Route path='/' element={<Homepage />} />
           <Route path='/product-list' element={<Productlist />} />
           <Route path='/cart' element={<Cart/>}/>
+          <Route path='/promos' element={<Promos/>}/>
       </Routes>
       <Footer/>
     </CartProvider>
